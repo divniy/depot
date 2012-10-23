90a994a91a0ac1082f41eca496a7d488bafdec0a
Dmitriy Paramonov <divniy@gmail.com>
2012-10-23 15:06:07 +0400

    Add advanced cart. (end of 10)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,11 +14,14 @@ class CartsController < ApplicationController
   # GET /carts/1.json
   def show
     @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
-    end
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Try access to invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
   end
 
   # GET /carts/new
@@ -72,11 +75,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product, :product_id
+  attr_accessible :cart_id, :product, :product_id, :quantity
   belongs_to :cart
   belongs_to :product
+
+  def total_price
+    quantity * product.price
+  end
 end
